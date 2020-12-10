6b873e4c9ce8c27e50f4d4f76c0a4d6907f32a7e
Hinako-Tanaka <youngbird1228@gmail.com>
2020-12-10 21:52:08 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    echo 'Hello';
+// 1 クラス
+    Class Car {
+        // 3　プロパティ
+        $speed;
+        $exhaustgas;
+        $color;
+
+        // 4 メソッド（関数・技）
+        function run(){
+
+        }
+        function deliver(){
+
+        }
+        
+    }
+
+// 2 インスタンスをうむ
+    $car = new Car('トヨタ');
+    $car = new Car('スバル');
+    
